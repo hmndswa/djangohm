adf3313c04031c9a015034d62ea0005a205a160a
hasith@wireapps <hasithwireapps@Hasiths-MacBook-Pro.local>
2025-07-30 02:54:55 +0100

    Model Instance as an API response

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,24 @@
+import json 
 from django.http import JsonResponse
 
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "Hi there, this is your Django API response"})
+    #request -> HttpRequest -> from Django itself
+    #alternatively print(dir(request))
+    #request.body
+    print(request.GET)  #url query params 
+    body = request.body #byte string of JSON data
+    data = {}
+    try:
+        data = json.loads(body)
+    except:
+        pass
+    print(data)
+    #data['headers'] = request.headers 
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+
+    return JsonResponse(data)
+
 
--- py_client/basic.py
+++ py_client/basic.py
@@ -4,7 +4,7 @@ import requests
 #endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8000/api/"       #based off django
 
-get_response = requests.get(endpoint, json={"query": "Hello world"}) #HTTP Request
+get_response = requests.get(endpoint,params={"abc": 123}, json={"query": "Hello world"}) #HTTP Request
 print(get_response.text) #print the raw text response
 print(get_response.status_code)
 
